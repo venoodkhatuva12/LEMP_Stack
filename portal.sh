c68a381603bbd3e1e539d127f84de146c1eab5ff
Vinood Katuwa <vinod.katuwa12@gmail.com>
2017-04-13 14:17:07 +0530

    Update portal.sh

--- portal.sh
+++ portal.sh
@@ -16,8 +16,8 @@ sudo yum install wget && wget http://repo.mysql.com/mysql-community-release-el6-
 sudo rpm -ivh mysql-community-release-el6-5.noarch.rpm
 echo "Installing the Remi Repo..."
 wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm && rpm -Uvh epel-release-latest-6.noarch.rpm
-sudo sed -i 's/enabled=0/enabled=1/g' /etc/yum.repos.d/remi.repo
 sudo rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
+sudo sed -i 's/enabled=0/enabled=1/g' /etc/yum.repos.d/remi.repo
 
 # Yum update with new repo
 sudo yum --enablerepo=remi,remi-php56 update -y && sudo yum --enablerepo=remi,remi-php56 upgrade -y
